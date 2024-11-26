2a2e6fe59ea33539f747bd60df993b96eef86a1f
mega-haste <www.hichamhot@gmail.com>
2024-11-26 12:04:19 +0100

    Update

---
+++ sync.py
@@ -0,0 +1,35 @@
+import os
+from os.path import isdir, isfile
+import shutil
+
+SYNC_LIST = [
+    ".bashrc",
+    ".tmux.conf",
+    ".fonts/",
+    # ".tmux/",
+    # ".config/nvim/",
+    ".config/yazi/",
+    ".config/i3/",
+    ".config/sway/",
+    ".config/pip/",
+    ".config/neofetch/",
+    ".config/fuzzel/",
+    ".config/fontconfig/",
+    ".config/fish/",
+    ".config/flameshot/",
+]
+
+for _f in SYNC_LIST:
+    f = os.path.expanduser(os.path.join("~", _f))
+    here = os.path.dirname(os.path.abspath(__file__))
+    dist = os.path.join(here, _f)
+    if not os.path.exists(f):
+        print(f"'{f}' doesn't exist.")
+        continue
+    if isfile(f):
+        print(f"Copying the file '{f}' to '{dist}'...")
+        shutil.copy(f, dist)
+    elif isdir(f):
+        print(f"Copying the dir '{f}' to '{dist}'...")
+        shutil.copytree(f, dist, dirs_exist_ok=True)
+
